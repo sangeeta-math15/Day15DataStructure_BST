1824c4804586956256ed89f9571d4fa54c85dfc7
sngtmth <sngtmth@gmail.com>
2021-10-16 00:11:53 +0530

    Added:Create Binary Search Tree to add the elements

---
+++ src/main/java/com/bridgelabz/MyBinaryNode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class MyBinaryNode<K extends Comparable<K>> {
+    K key;
+    public MyBinaryNode<K> left;
+    public MyBinaryNode<K> right;
+
+    public MyBinaryNode(K key) {
+        this.key=key;
+        this.left= left;
+        this.right=right;
+    }
+}
---
+++ src/test/java/com/bridgelabz/MyBinaryTreeTest.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+    @Test
+    public void givenNumbers__WhenAddedToBST_ShouldReturnSize()
+    {
+        MyBinaryTree<Integer> myBinaryTree=new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size=myBinaryTree.getSize();
+        Assert.assertEquals(3,size);
+    }
+}
